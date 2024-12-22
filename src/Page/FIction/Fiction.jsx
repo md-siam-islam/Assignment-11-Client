c2ab033cfe97cc504076854c6f964cdb28eefd74
md-siam-islam <mdsiamislam663@gmail.com>
2024-12-22 12:41:31 +0600

    fiction releted book add done

--- src/Page/FIction/Fiction.jsx
+++ src/Page/FIction/Fiction.jsx
@@ -3,6 +3,9 @@ import React from "react";
 import ReactStars from "react-rating-stars-component";
 
 import img from "../../../src/assets/image/The Great Gatsby.png"
+import img2 from "../../../src/assets/image/To Kill a Mockingbirdjpg.jpg"
+import img3 from "../../../src/assets/image/1984.jpg"
+
 import Navbar from "../../Components/Navbar/Navbar";
 import Footer from "../../Components/Footer/Footer";
 
@@ -26,7 +29,7 @@ const Fiction = () => {
           <h3 className="text-xl font-semibold text-gray-700 mb-2">
             The Great Gatsby
           </h3>
-          <p className="text-gray-500 mb-2">Author: F. Scott Fitzgerald</p>
+          <p className="text-gray-500 mb-2">Author : F. Scott Fitzgerald</p>
           <div className="flex items-center mb-4">
             <ReactStars
               count={5}
@@ -41,6 +44,57 @@ const Fiction = () => {
             Details
           </button>
         </div>
+
+        <div className="bg-white shadow-md rounded-lg p-4 hover:shadow-lg transition">
+          <img
+            src={img2}
+            alt="img"
+            className="w-full h-72 object-cover rounded-md mb-4"
+          />
+          <h3 className="text-xl font-semibold text-gray-700 mb-2">
+            To Kill a Mockingbird
+          </h3>
+          <p className="text-gray-500 mb-2">Author : Harper Lee</p>
+          <div className="flex items-center mb-4">
+            <ReactStars
+              count={5}
+              value="4.8"
+              size={24}
+              edit={false}
+              activeColor="#ffd700"
+            />
+            <span className="ml-2 text-gray-600">4.8</span>
+          </div>
+          <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition">
+            Details
+          </button>
+        </div>
+
+        <div className="bg-white shadow-md rounded-lg p-4 hover:shadow-lg transition">
+          <img
+            src={img3}
+            alt="img"
+            className="w-full h-72 object-cover rounded-md mb-4"
+          />
+          <h3 className="text-xl font-semibold text-gray-700 mb-2">
+            1984
+          </h3>
+          <p className="text-gray-500 mb-2">Author : George Orwell</p>
+          <div className="flex items-center mb-4">
+            <ReactStars
+              count={5}
+              value="4.8"
+              size={24}
+              edit={false}
+              activeColor="#ffd700"
+            />
+            <span className="ml-2 text-gray-600">4.7</span>
+          </div>
+          <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition">
+            Details
+          </button>
+        </div>
+
 
       </div>
     </div>
